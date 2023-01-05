95b88047af69841dcf388c6b6ba93b8982181736
Spencer Haka <spencer.haka@gmail.com>
2023-01-05 12:31:43 -0700

    g

--- knexfile.js
+++ knexfile.js
@@ -14,19 +14,15 @@ module.exports = {
     }
   },
   production: {
-    client: 'postgresql',
-    connection: {
-      connectionString: process.env.DATABASE_URL + `?ssl=true`,
-      ssl: {
-       rejectUnauthorized: false,
-      },
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './migrations'
     },
   },
 };
