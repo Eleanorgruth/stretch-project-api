5d9e579f7a721a19e16c790830dfb6b4b6e84179
Spencer Haka <spencer.haka@gmail.com>
2023-01-05 12:59:48 -0700

    Remove 'ssl=true' from knexfile connection string

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      connectionString: process.env.DATABASE_URL + `?ssl=true`,
+      connectionString: process.env.DATABASE_URL,
       ssl: {
        rejectUnauthorized: false,
       },
