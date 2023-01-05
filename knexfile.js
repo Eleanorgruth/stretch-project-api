c097e546f1c795ee9b05a9856ec8ed83da57b57d
Spencer Haka <spencer.haka@gmail.com>
2023-01-05 12:41:45 -0700

    Trying to deploy

--- knexfile.js
+++ knexfile.js
@@ -14,15 +14,19 @@ module.exports = {
     }
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + `?ssl=true`,
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL + `?ssl=true`,
+      ssl: {
+       rejectUnauthorized: false,
+      },
+    },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations',
-      directory: './migrations'
+      tableName: 'knex_migrations'
     },
   },
 };
