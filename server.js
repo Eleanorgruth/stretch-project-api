43a8ab5160866c5df78e4a208d43916207db0ea4
Eleanor Ruth <ellie.ruth@me.com>
2023-01-05 21:21:16 -0700

    Clean up comments

--- server.js
+++ server.js
@@ -2,31 +2,28 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const knex = require('./knex')
-
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 app.set('port', 8080)
 
-app.use(cors())
-
 //Get all comics in collection
-app.get('/api/v1/comicData', async(request, response) => {
+app.get('/api/v1/comicData', async (request, response) => {
   const comicData = await knex.select().from('comicData')
   response.status(200).json(comicData)
 })
 
 //Get single comic in collection
 app.get('/api/v1/comicData/:id', async (request, response) => {
-
-  try{
+  try {
     const { id } = request.params
     const comic = await knex.where('id', id).select().from('comicData')
-  
+
     if (comic.length) {
       response.status(200).json(comic)
-      
+
     } else {
       response.status(404).send(`Comic not found matching the id ${id}`)
     }
@@ -36,7 +33,7 @@ app.get('/api/v1/comicData/:id', async (request, response) => {
 })
 
 //Add new comic to collection
-app.post('/api/v1/comicData', async(request, response) => {
+app.post('/api/v1/comicData', async (request, response) => {
   for (let requiredParameter of ['id', 'title', 'year', 'issue', 'grade', 'image_path', 'verified', 'note']) {
     if (!request.body[requiredParameter]) {
       return response
@@ -53,10 +50,9 @@ app.post('/api/v1/comicData', async(request, response) => {
 })
 
 //Update single comic in collection
-
 app.put('/api/v1/comicData/:id', async (request, response) => {
   try {
-   const comic = await knex('comicData').where('id', Number(request.params.id)).update(request.body, ['id', 'title', 'year', 'issue', 'grade', 'image_path', 'verified', 'note'])
+    const comic = await knex('comicData').where('id', Number(request.params.id)).update(request.body, ['id', 'title', 'year', 'issue', 'grade', 'image_path', 'verified', 'note'])
     response.status(200).json(comic[0])
   } catch (error) {
     response.status(500).json(error)
@@ -67,7 +63,7 @@ app.put('/api/v1/comicData/:id', async (request, response) => {
 app.delete('/api/v1/comicData/:id', async (request, response) => {
   try {
     await knex('comicData').where('id', Number(request.params.id)).del()
-    response.status(200).json({response: `Comic with id:${request.params.id} was deleted`})
+    response.status(200).json({ response: `Comic with id:${request.params.id} was deleted` })
   } catch (error) {
     response.status(500).json(error)
   }
